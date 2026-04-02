0febd5c0f6252b58027f9bdc62b517876e771156
Klaus Kristensen <klausiboy@gmail.com>
2026-04-02 21:26:42 +0200

    Documentation of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,33 @@ int main() {
         return 1;
     }
 
+    // Instantiate bot with token
     dpp::cluster bot(BOT_TOKEN);
  
+    // Behavior triggered by log-event: forward log entry to console.
     bot.on_log(dpp::utility::cout_logger());
  
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-        if (event.command.get_command_name() == "ping") {
+    // Behavior triggered by slash commands 
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) 
+    {
+        // define /ping command behavior: reply with "Pong!" in chat
+        if (event.command.get_command_name() == "ping") 
+        {
             event.reply("Pong!");
         }
     });
  
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        if (dpp::run_once<struct register_bot_commands>()) {
+    // Behavior triggered when the bot is ready
+    bot.on_ready([&bot](const dpp::ready_t& event) 
+    {
+        // run_once ensures ensures no re-registration of commands.
+        if (dpp::run_once<struct register_bot_commands>()) 
+        {
+            // create ping-command in API
             bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
         }
     });
- 
+
+    // Start the bot
     bot.start(dpp::st_wait);
 }
